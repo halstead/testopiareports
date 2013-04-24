61ecb7552aa4dea24fb732a30d73822397715f92
Stoicescu Cornel <corneliux.stoicescu@intel.com>
2013-04-24 14:01:16 +0000

    -improoved Concatstats report % calculus by delaying the point in which actual values are transformed into percents(%)

--- TR_repConcatstats.php
+++ TR_repConcatstats.php
@@ -111,33 +111,35 @@ $sqlBLOCKED ="select * from (".$sql->toSQL().") as temp_table GROUP BY temp_tabl
 #EDITED: The below section also treats a "NULL" Test Plan and Environment scenario. This was present in a previous implementation of this report but now it always returns the correct test plan and environment names.
 	if ($run_counter == 0) {
 		$result = "SELECT IFNULL(table1.product_id,IFNULL(table2.product_id,IFNULL(table3.product_id,\"NOT READY: $run_id\"))) as product_id, 
-		 IFNULL(table1.run_id,IFNULL(table2.run_id,IFNULL(table3.run_id,\"NOT READY: $run_id\"))) as run_id,
-		 IFNULL(table1.Test_Plan,IFNULL(table2.Test_Plan,IFNULL(table3.Test_Plan,\"NOT READY: $run_id\"))) AS \"Test_Plan\", 
-		 IFNULL(table1.Environment,IFNULL(table2.Environment,IFNULL(table3.Environment,\"NOT READY: $run_id\"))) AS \"Environment\",
-		 IFNULL(FORMAT((table1.status*100)/(table1.status+table2.status+table3.status),1),0) AS Passed, table1.bugs as \"Other_issues\", 
-		 IFNULL(FORMAT((table2.status*100)/(table1.status+table2.status+table3.status),1),0) AS Failed, table2.bugs as \"Failing_bugs\", 
-		 IFNULL(FORMAT((table3.status*100)/(table1.status+table2.status+table3.status),1),0) AS Blocked, table3.bugs as \"Blocking_bugs\" 
+		 IFNULL(table1.run_id,IFNULL(table2.run_id,IFNULL(table3.run_id,\"NOT READY\"))) as run_id,
+		 IFNULL(table1.Test_Plan,IFNULL(table2.Test_Plan,IFNULL(table3.Test_Plan,\"NOT READY\"))) AS \"Test_Plan\", 
+		 IFNULL(table1.Environment,IFNULL(table2.Environment,IFNULL(table3.Environment,\"NOT READY\"))) AS \"Environment\",
+		 table1.status AS Passed, table1.bugs as \"Other_issues\", 
+		 table2.status AS Failed, table2.bugs as \"Failing_bugs\", 
+		 table3.status AS Blocked, table3.bugs as \"Blocking_bugs\" 
 		FROM ($sqlPASSED) AS table1 INNER JOIN ($sqlFAILED) as table2 ON table1.run_id=table2.run_id INNER JOIN ($sqlBLOCKED) as table3 ON table1.run_id=table3.run_id";
 		$run_counter++;
 	}
 	else {
-		$result .= " UNION ALL SELECT IFNULL(table1.product_id,IFNULL(table2.product_id,IFNULL(table3.product_id,\"NOT READY: $run_id\"))) as product_id,
-		 IFNULL(table1.run_id,IFNULL(table2.run_id,IFNULL(table3.run_id,\"NOT READY: $run_id\"))) as run_id,
-		 IFNULL(table1.Test_Plan,IFNULL(table2.Test_Plan,IFNULL(table3.Test_Plan,\"NOT READY: $run_id\"))) AS \"Test_Plan\",
-                 IFNULL(table1.Environment,IFNULL(table2.Environment,IFNULL(table3.Environment,\"NOT READY: $run_id\"))) AS \"Environment\",
-                 IFNULL(ROUND((table1.status*100)/(table1.status+table2.status+table3.status),1),0) AS Passed, table1.bugs as \"Other_issues\",
-                 IFNULL(ROUND((table2.status*100)/(table1.status+table2.status+table3.status),1),0) AS Failed, table2.bugs as \"Failing_bugs\",
-                 IFNULL(ROUND((table3.status*100)/(table1.status+table2.status+table3.status),1),0) AS Blocked, table3.bugs as \"Blocking_bugs\"
-                FROM ($sqlPASSED) AS table1 INNER JOIN ($sqlFAILED) as table2 ON table1.run_id=table2.run_id INNER JOIN ($sqlBLOCKED) as table3 ON table1.run_id=table3.run_id";
+		$result .= " UNION ALL SELECT IFNULL(table1.product_id,IFNULL(table2.product_id,IFNULL(table3.product_id,\"NOT READY: $run_id\"))) as product_id, 
+		 IFNULL(table1.run_id,IFNULL(table2.run_id,IFNULL(table3.run_id,\"NOT READY\"))) as run_id, 
+		 IFNULL(table1.Test_Plan,IFNULL(table2.Test_Plan,IFNULL(table3.Test_Plan,\"NOT READY\"))) AS \"Test_Plan\", 
+		 IFNULL(table1.Environment,IFNULL(table2.Environment,IFNULL(table3.Environment,\"NOT READY\"))) AS \"Environment\",
+		 table1.status AS Passed, table1.bugs as \"Other_issues\", 
+		 table2.status AS Failed, table2.bugs as \"Failing_bugs\", 
+		 table3.status AS Blocked, table3.bugs as \"Blocking_bugs\" 
+		FROM ($sqlPASSED) AS table1 INNER JOIN ($sqlFAILED) as table2 ON table1.run_id=table2.run_id INNER JOIN ($sqlBLOCKED) as table3 ON table1.run_id=table3.run_id";
 
+
+	}
 }
-}
-		return "SELECT GROUP_CONCAT(temp_table.run_id) as \"Test Run\", temp_table.Test_Plan as \"Test Plan\", temp_table.Environment, 
-			ROUND(AVG(temp_table.Passed),1) as Passed, GROUP_CONCAT(temp_table.Other_issues) as \"Other issues\",
-			ROUND(AVG(temp_table.Failed),1) as Failed, GROUP_CONCAT(temp_table.Failing_bugs) as \"Failing bugs\", 
-			ROUND(AVG(temp_table.Blocked),1) as Blocked, GROUP_CONCAT(temp_table.Blocking_bugs) as \"Blocking bugs\"
-			FROM ($result) as temp_table INNER JOIN $products ON $products.id=temp_table.product_id GROUP BY temp_table.Test_Plan, temp_table.Environment 
-			ORDER BY ($products.classification_id+0) ASC, (temp_table.product_id+0) DESC, temp_table.Environment";
+
+return "SELECT GROUP_CONCAT(temp_table.run_id) as \"Test Run\", temp_table.Test_Plan as \"Test Plan\", temp_table.Environment, 
+        IFNULL(FORMAT(AVG(temp_table.Passed*100)/(AVG(temp_table.Passed)+AVG(temp_table.Failed)+AVG(temp_table.Blocked)),1),0) AS Passed, GROUP_CONCAT(temp_table.Other_issues) as \"Other issues\", 
+        IFNULL(FORMAT(AVG(temp_table.Failed*100)/(AVG(temp_table.Passed)+AVG(temp_table.Failed)+AVG(temp_table.Blocked)),1),0) AS Failed, GROUP_CONCAT(temp_table.Failing_bugs) as \"Failing bugs\", 
+        IFNULL(FORMAT(AVG(temp_table.Blocked*100)/(AVG(temp_table.Passed)+AVG(temp_table.Failed)+AVG(temp_table.Blocked)),1),0) AS Blocked, GROUP_CONCAT(temp_table.Blocking_bugs) as \"Blocking bugs\"
+        FROM ($result) as temp_table INNER JOIN $products ON $products.id=temp_table.product_id GROUP BY temp_table.Test_Plan, temp_table.Environment 
+        ORDER BY ($products.classification_id+0) ASC, (temp_table.product_id+0) DESC, temp_table.Environment";
 	}
 
 
